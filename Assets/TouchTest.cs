4ab1272549d1ef1b1e3a83d3078376c4f0016c44
Fayez Awad <awadfayez@gmail.com>
2020-05-22 11:32:07 -0400

    Initial Commit
    Initial Commit.
    Original file from old project./

---
+++ Assets/TouchTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchTest : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("unpdate");
+        if (Input.touchCount > 0)
+        {
+            Debug.Log("in 1");
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("in 2");
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    Debug.Log("Hit something");
+
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
